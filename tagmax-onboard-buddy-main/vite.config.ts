edd095df74416a9d85637b8adc5a2012cfbdf575
sbanyal-cmt <sbanyal@cmtelematics.com>
2025-11-30 03:03:25 -0500

    Update vite.config.ts

--- tagmax-onboard-buddy-main/vite.config.ts
+++ tagmax-onboard-buddy-main/vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/tagmax-onboard-buddy/" : "/", // 👈 add this
+  base: mode === "production" ? "/tagmax-install-validation/" : "/",
   server: {
     host: "::",
     port: 8080,
